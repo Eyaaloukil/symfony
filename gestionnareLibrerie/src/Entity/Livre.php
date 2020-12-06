a1b9e10ac4da7eb9c5bde5cd352531aef1b94c45
eya loukil <loukileya97@gmail.com>
2020-12-06 20:29:44 +0100

    first commit

---
+++ gestionnareLibrerie/src/Entity/Livre.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Date;
+
+/**
+ * @ORM\Entity(repositoryClass=LivreRepository::class)
+ */
+class Livre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbpages;
+
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbexemplaires;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $isbn;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Editeur::class, inversedBy="livres")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $editeur;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Categorie::class, inversedBy="livres")
+     */
+    private $categorie;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Auteur::class, inversedBy="livres")
+     */
+    private $auteurs;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dateedition;
+
+    public function __construct()
+    {
+        $this->auteurs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getNbpages(): ?int
+    {
+        return $this->nbpages;
+    }
+
+    public function setNbpages(int $nbpages): self
+    {
+        $this->nbpages = $nbpages;
+
+        return $this;
+    }
+
+    public function getDateedition(): ?\DateTimeInterface
+    {
+        return $this->dateedition;
+    }
+
+    public function setDateedition(\DateTimeInterface $dateedition): self
+    {
+        $this->dateedition = $dateedition;
+
+        return $this;
+    }
+
+    public function getNbexemplaires(): ?int
+    {
+        return $this->nbexemplaires;
+    }
+
+    public function setNbexemplaires(int $nbexemplaires): self
+    {
+        $this->nbexemplaires = $nbexemplaires;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getEditeur(): ?Editeur
+    {
+        return $this->editeur;
+    }
+
+    public function setEditeur(?Editeur $editeur): self
+    {
+        $this->editeur = $editeur;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Auteur[]
+     */
+    public function getAuteurs(): Collection
+    {
+        return $this->auteurs;
+    }
+
+    public function addAuteur(Auteur $auteur): self
+    {
+        if (!$this->auteurs->contains($auteur)) {
+            $this->auteurs[] = $auteur;
+        }
+
+        return $this;
+    }
+
+    public function removeAuteur(Auteur $auteur): self
+    {
+        $this->auteurs->removeElement($auteur);
+
+        return $this;
+    }
+
+}
